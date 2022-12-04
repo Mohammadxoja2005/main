ad2a20adbc947725edd464f470fff16e4e43ba61
Muhammadxoja <rustamxojayev.mohammadxoja@yandex.ru>
2022-12-04 22:44:46 +0500

    some changes

---
+++ models/Quote.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => { 
+  
+    const Quote = sequelize.define("Quote", {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    });
+    return Quote;
+  };
+  
